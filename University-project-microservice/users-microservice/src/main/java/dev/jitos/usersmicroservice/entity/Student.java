42e9490d1df2e6627955fc96f20664d47e1a3c4c
Juan José García <jitos86@gmail.com>
2022-12-10 19:36:44 +0100

    add entity Student

---
+++ University-project-microservice/users-microservice/src/main/java/dev/jitos/usersmicroservice/entity/Student.java
@@ -0,0 +1,32 @@
+package dev.jitos.usersmicroservice.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "students")
+@Getter @Setter
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    @Column(name = "create_at")
+    private Date createAt;
+
+    /**
+     * Con @PrePersist ejecutamos el código del método antes de guardar en BBDD. Para
+     * este ejemplo inicializamos la fecha
+     */
+    @PrePersist
+    public void assingDate(){
+        this.createAt = new Date();
+    }
+
+}
