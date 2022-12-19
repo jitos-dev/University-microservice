ace1d3b25aa019ea5710a9a86a8c3897c1541778
Juan José García <jitos86@gmail.com>
2022-12-19 20:14:10 +0100

    generic controller and extends in StudentController

--- University-project-microservice/commons-microservice/src/main/java/dev/jitos/commonsmicroservice/controller/GenericController.java
+++ University-project-microservice/commons-microservice/src/main/java/dev/jitos/commonsmicroservice/controller/GenericController.java
@@ -1,24 +1,28 @@
 package dev.jitos.commonsmicroservice.controller;
+import dev.jitos.commonsmicroservice.service.GenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-/*No le ponemos path porque se la vamos a signar en el Api Gateway Zuul*/
-@RestController
-@RequestMapping("students")
-public class GenericController {
-/*
-    @Autowired
-    @Qualifier("studentServiceImpl")
-    private StudentService service;
+/*No le ponemos @RestController porque es genérico.
+* El método de editar no lo hacemos genérico porque depende de los getters y setters del objeto
+* Las iniciales de la parte genérica son: E para la entidad y S para el service
+* El service lo hacemos protected para poder utilizarlo en las clases que hereden de esta*/
+public class GenericController<E, S extends GenericService<E>> {
 
-    *//**
+    @Autowired
+    protected S service;
+
+    /**
      * Anotamos este método con @GetMapping para indicar que lo llamamos por get y no le indicamos
      * ninguna url porque lo llamamos desde la raíz del controlador. Le indicamos un parámetro genérico
      * con la interrogación para indicar que puede ser de cualquier cosa.
      * @return indicamos que tiene que devolver una respuesta ok (status 200) y la guardamos en el body que convierte
      * automáticamente lo que devuelva (puede ser un List, objeto, etc) a formato Json
-     *//*
+     */
     @GetMapping
     public ResponseEntity<?> findAll() {
         return ResponseEntity.ok().body(service.findAll());
@@ -26,7 +30,7 @@ public class GenericController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
-        Optional<Student> optional = service.findById(id);
+        Optional<E> optional = service.findById(id);
 
         if (optional.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -35,45 +39,20 @@ public class GenericController {
     }
 
     @PostMapping
-    public ResponseEntity<?> save(@RequestBody Student student) {
-        Optional<Student> optional = service.save(student);
+    public ResponseEntity<?> save(@RequestBody E entity) {
+        Optional<E> optional = service.save(entity);
         return ResponseEntity.status(HttpStatus.CREATED).body(optional.isPresent());
     }
 
-    *//**
-     * Primero obtenemos el objeto de la entidad de la bbdd para ver si existe. Si no existe retornamos
-     * un notFound y si existe mapeamos el objeto que nos pasan por parámetro con el del Optional cambiando
-     * los valores que queramos (el id ya lo trae el optional). Por último retornamos el status pero en el
-     * body llamamos a save del objeto que hemos mapeado (para el ejemplo studentDB)
-     * @param student objeto de la entidad Student
-     * @param id id del objeto que queremos modificar
-     * @return respuesta que recibimos al guardarlo
-     *//*
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@RequestBody Student student, @PathVariable Long id) {
-        Optional<Student> optional = service.findById(id);
 
-        if (optional.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        //Modificamos los valores del objeto que queramos cambiar. studentDB ya tiene el id
-        Student studentDB = optional.get();
-        studentDB.setName(student.getName());
-        studentDB.setSurname(student.getSurname());
-        studentDB.setEmail(student.getEmail());
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(studentDB));
-    }
-
-    *//**
+    /**
      * En este como el método de borrar no devuelve nada solo devolvemos un ResonseEntity.noContent
      * @param id id del objeto que queremos borrar
      * @return ResponseEntity.noContent() (status 204)
-     *//*
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
-    }*/
+    }
 }
--- University-project-microservice/users-microservice/src/main/java/dev/jitos/usersmicroservice/controller/StudentController.java
+++ University-project-microservice/users-microservice/src/main/java/dev/jitos/usersmicroservice/controller/StudentController.java
@@ -1,6 +1,8 @@
 package dev.jitos.usersmicroservice.controller;
 
+import dev.jitos.commonsmicroservice.controller.GenericController;
 import dev.jitos.usersmicroservice.entity.Student;
+import dev.jitos.usersmicroservice.impl.StudentServiceImpl;
 import dev.jitos.usersmicroservice.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,39 +15,39 @@ import java.util.Optional;
 /*No le ponemos path porque se la vamos a signar en el Api Gateway Zuul*/
 @RestController
 @RequestMapping("students")
-public class StudentController {
+public class StudentController extends GenericController<Student, StudentService> {
 
-    @Autowired
-    @Qualifier("studentServiceImpl")
-    private StudentService service;
+//    @Autowired
+//    @Qualifier("studentServiceImpl")
+//    private StudentService service;
 
-    /**
-     * Anotamos este método con @GetMapping para indicar que lo llamamos por get y no le indicamos
-     * ninguna url porque lo llamamos desde la raíz del controlador. Le indicamos un parámetro genérico
-     * con la interrogación para indicar que puede ser de cualquier cosa.
-     * @return indicamos que tiene que devolver una respuesta ok (status 200) y la guardamos en el body que convierte
-     * automáticamente lo que devuelva (puede ser un List, objeto, etc) a formato Json
-     */
-    @GetMapping
-    public ResponseEntity<?> findAll() {
-        return ResponseEntity.ok().body(service.findAll());
-    }
+//    /**
+//     * Anotamos este método con @GetMapping para indicar que lo llamamos por get y no le indicamos
+//     * ninguna url porque lo llamamos desde la raíz del controlador. Le indicamos un parámetro genérico
+//     * con la interrogación para indicar que puede ser de cualquier cosa.
+//     * @return indicamos que tiene que devolver una respuesta ok (status 200) y la guardamos en el body que convierte
+//     * automáticamente lo que devuelva (puede ser un List, objeto, etc) a formato Json
+//     */
+//    @GetMapping
+//    public ResponseEntity<?> findAll() {
+//        return ResponseEntity.ok().body(service.findAll());
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<?> findById(@PathVariable Long id) {
+//        Optional<Student> optional = service.findById(id);
+//
+//        if (optional.isEmpty()) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok(optional.get());
+//    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id) {
-        Optional<Student> optional = service.findById(id);
-
-        if (optional.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(optional.get());
-    }
-
-    @PostMapping
-    public ResponseEntity<?> save(@RequestBody Student student) {
-        Optional<Student> optional = service.save(student);
-        return ResponseEntity.status(HttpStatus.CREATED).body(optional.isPresent());
-    }
+//    @PostMapping
+//    public ResponseEntity<?> save(@RequestBody Student student) {
+//        Optional<Student> optional = service.save(student);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(optional.isPresent());
+//    }
 
     /**
      * Primero obtenemos el objeto de la entidad de la bbdd para ver si existe. Si no existe retornamos
@@ -73,14 +75,14 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(studentDB));
     }
 
-    /**
-     * En este como el método de borrar no devuelve nada solo devolvemos un ResonseEntity.noContent
-     * @param id id del objeto que queremos borrar
-     * @return ResponseEntity.noContent() (status 204)
-     */
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
-        service.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
+//    /**
+//     * En este como el método de borrar no devuelve nada solo devolvemos un ResonseEntity.noContent
+//     * @param id id del objeto que queremos borrar
+//     * @return ResponseEntity.noContent() (status 204)
+//     */
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<?> delete(@PathVariable Long id) {
+//        service.deleteById(id);
+//        return ResponseEntity.noContent().build();
+//    }
 }
